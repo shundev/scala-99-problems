785c1fd9cc043e2acb46fa9759ab0bf04e74b5d6
Shunsuke HONDO <shunsuke.hondo@gmail.com>
2019-04-28 20:34:58 +0900

    init P01

---
+++ src/main/scala/P01.scala
@@ -0,0 +1,14 @@
+// 01
+// Find the last element of a list.
+// Example:
+// scala> last(List(1, 1, 2, 3, 5, 8))
+// res0: Int = 8
+
+object P01 {
+  def last[T](list: Seq[T]): T = list match {
+    case x :: Nil => x
+    case x :: xs  => last(xs)
+    case _        => sys.error("last isn't defined for empty list")
+  }
+}
+
