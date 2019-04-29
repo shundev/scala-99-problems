caec7b76a77badc21f02f8a305cf258a30221dcf
Shunsuke HONDO <shunsuke.hondo@gmail.com>
2019-04-29 21:29:35 +0900

    [wip] Add 06

--- src/main/scala/P05.scala
+++ src/main/scala/P05.scala
@@ -9,6 +9,12 @@ import scala.annotation.tailrec
 
 object P05 {
   def reverse[T](list: List[T]): List[T] = {
+    @tailrec
+    def rec[T](lst: List[T], acc: List[T]): List[T] = lst match {
+      case Nil => acc
+      case x::xs => rec(xs, x :: acc)
+    }
 
+    rec(list, Nil)
   }
 }
---
+++ src/main/scala/P06.scala
@@ -0,0 +1,13 @@
+// P06
+// Find out whether a list is a palindrome.
+// Example:
+// scala> isPalindrome(List(1, 2, 3, 2, 1))
+// res0: Boolean = true
+
+import scala.annotation.tailrec;
+
+object P06 {
+  def isPalindrome[T](list: List[T]): Boolean = list match {
+    case _ => true;
+  }
+}
